1dca24b208b648e640d3e4e843f9b786fbfdf573
Nico Nistal <nicolas.nistal@thegoneapp.com>
2015-09-09 22:55:35 -0300

    todomvc added

--- src/react-speech-based-mixin.js
+++ src/react-speech-based-mixin.js
@@ -1,6 +1,18 @@
 var voiceCommandDispatcher = require('./vendor/voice-command-dispatcher/dist/vcd.js');
 var ReactSpeechBasedMixin = {
+    listenForIntent: function(intent) {
+        //Listens for the specific intent from the WitDispatcher.
+        // WitDispatcher.subscribe(intent, this.processIntent);
+        this.processIntent();
+    },
 
+    processIntent: function(data) {
+        //Process the intent, this method has to be redefined by the component
+    },
+
+    componentWillMount: function() {
+        console.log('MIXIN');
+    }
 };
 
 module.exports = ReactSpeechBasedMixin;
--- src/vendor/voice-command-dispatcher/sample-app/app.js
+++
@@ -1,7 +0,0 @@
-var VoiceCommandDispatcher = require('../src/voice-command-dispatcher');
-var witService = require('../src/modules/service-layer/wit-xhr-service-layer');
-var voiceChannel = new VoiceCommandDispatcher(witService);
-
-window.document.querySelector('.js-trigger-mic').addEventListener('click', function() {
-    voiceChannel.start();
-});
